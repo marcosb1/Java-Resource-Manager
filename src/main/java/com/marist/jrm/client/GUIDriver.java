998cec4165464327f7dd5563ec09b8be60ef9866
marcosb1 <Marcos.Barbieri1@marist.edu>
2018-03-25 15:50:22 -0400

    Adding tabs for window + creating models for data

--- src/main/java/com/marist/jrm/client/GUIDriver.java
+++ src/main/java/com/marist/jrm/client/GUIDriver.java
@@ -3,12 +3,12 @@ package com.marist.jrm.client;
 import com.marist.jrm.client.components.ConfirmBox;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class GUIDriver extends Application {
@@ -43,9 +43,15 @@ public class GUIDriver extends Application {
 
         /* File Menu initialization and items */
         Menu fileMenu = new Menu("File");
-        fileMenu.getItems().add(new MenuItem("Export..."));
+
+        MenuItem export = new MenuItem("Export...");
+        export.setDisable(false);
+        fileMenu.getItems().add(export);
+
         fileMenu.getItems().add(new SeparatorMenuItem());
+
         MenuItem close = new MenuItem("Close");
+        close.setDisable(false);
         close.setOnAction(e -> {
             e.consume();
             this.closeProgram();
@@ -53,12 +59,47 @@ public class GUIDriver extends Application {
         fileMenu.getItems().add(close);
 
         menu.getMenus().addAll(fileMenu);
+        /* End of File Menu initialization */
+
+        /* Tab Pane initialization */
+        TabPane tabPane = new TabPane();
+        Tab processesTab = new Tab("Processes");
+        processesTab.setClosable(false);
+        processesTab.setContent(new Rectangle(600, 700, Color.LIGHTGREY));
+
+        TableColumn<Process, String> processNameCol = new TableColumn<>("Process Name");
+        processNameCol.setMinWidth(200);
+        processNameCol.setCellValueFactory(new PropertyValueFactory<>("processName"));
+
+        TableColumn<Process, String> memoryCol = new TableColumn<>("Memory");
+        memoryCol.setMinWidth(200);
+        memoryCol.setCellValueFactory(new PropertyValueFactory<>("memory"));
+
+        TableColumn<Process, String> threadCountCol = new TableColumn<>("Thread Count");
+        threadCountCol.setMinWidth(200);
+        threadCountCol.setCellValueFactory(new PropertyValueFactory<>("threadCount"));
+
+        TableColumn<Process, String> descriptionCol = new TableColumn<>("Description");
+        descriptionCol.setMinWidth(200);
+        descriptionCol.setCellValueFactory(new PropertyValueFactory<>("description"));
+
+        TableView<Process> processTable = new TableView<>();
+        processTable.getColumns().addAll(processNameCol, memoryCol, threadCountCol, descriptionCol);
+        processesTab.setContent(processTable);
+        tabPane.getTabs().add(processesTab);
+
+        Tab performanceTab = new Tab("Performance");
+        performanceTab.setClosable(false);
+        performanceTab.setContent(new Rectangle(600, 700, Color.LIGHTGREY));
+        tabPane.getTabs().add(performanceTab);
+        /* End of Tab Pane initialization */
 
         this.layout = new BorderPane();
         this.layout.setTop(menu);
+        this.layout.setCenter(tabPane);
         // we must initialize the scene locally
         // TODO: which is better for a scene object, private or public
-        Scene applicationScene = new Scene(this.layout, 400, 300);
+        Scene applicationScene = new Scene(this.layout, 600, 800);
         this.applicationWindow.setScene(applicationScene);
         this.applicationWindow.show();
     }
