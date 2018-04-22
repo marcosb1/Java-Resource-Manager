6da454f94541b336a60202eb538760ee0309c9ca
Dominic <domrossillo@optonline.net>
2018-04-22 17:03:48 -0400

    added comments and reformated application code for code review

--- src/main/java/com/marist/jrm/application/ApplicationDriver.java
+++ src/main/java/com/marist/jrm/application/ApplicationDriver.java
@@ -1,13 +1,11 @@
 package com.marist.jrm.application;
 
 public class ApplicationDriver {
-    int heartBeatInteval=1;
-    public static void main(String Args[]){
-        //Connection conn= SQLiteJDBCDriverConnection.connect();
-        SQLiteDBInit.initDB();
-        SQLiteDBUtil util=new SQLiteDBUtil();
-        //util.insertApplication("Dom test", "Dom desc");
 
+
+    public static void main(String Args[]){
+        //initialize the database at startup
+        SQLiteDBInit.initDB();
 
     }
   
